8e0561dff2dbf47073d20cd46433539e00125828
Ira11111 <mirina-0208@yandex.ru>
2025-08-07 17:57:15 +0300

    midleware added

---
+++ midlewares/gin/auth.go
@@ -0,0 +1,68 @@
+package grpc
+
+import (
+	"github.com/Ira11111/go-interceptors/jwt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	BearerAuthScopes = "bearerAuth.Scopes"
+)
+
+type AuthMiddleware struct {
+	key string
+}
+
+func NewAuthInterceptor(key string) *AuthMiddleware {
+	return &AuthMiddleware{key: key}
+}
+
+func (a *AuthMiddleware) JWTClaims() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, exists := c.Get(BearerAuthScopes); exists {
+			tokenStr := c.GetHeader("Authorization")
+			if tokenStr == "" {
+				log.Print("token empty")
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "token empty"})
+				return
+			}
+			tokenParts := strings.Split(tokenStr, " ")
+			if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+				log.Print("token format is invalid")
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token format"})
+				return
+			}
+			token := tokenParts[1]
+			userId, userRoles, err := jwt.ParseToken(token, a.key)
+			if err != nil {
+				log.Print("failed to parse token")
+				switch err {
+				case jwt.ErrInvalidPubKey:
+					log.Print("Invalid key")
+				case jwt.ErrInvalidSignature:
+					log.Print("Invalid token signature")
+				case jwt.ErrInvalidClaims:
+					log.Print("Invalid token claims")
+					c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token claims"})
+				case jwt.ErrInvalidToken:
+					log.Print("Expired token")
+					c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "expired token"})
+				case jwt.ErrInvalidClaimsField:
+					log.Print("Invalid token field")
+					c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token field"})
+				}
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+				return
+			}
+			log.Printf("token parsed successfully")
+			c.Set("userId", userId)
+			c.Set("userRoles", userRoles)
+			log.Printf("context info added")
+			c.Next()
+		}
+
+	}
+}
